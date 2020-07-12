5a35df86cf10e131e5294aac210bbcc96f53ac8a
flockbithamp <flockbithamp@gmail.com>
2020-07-12 16:27:45 -0400

    Assignment uploaded

--- class_1_coding_tuts/lecture_3.js
+++ class_1_coding_tuts/lecture_3.js
@@ -9,15 +9,20 @@ var doo = "Hello World!";
 
 
 // Exercise 3: Take name, Roll no, mobile no. & email-id from the user and save it in a variable.
-var name = prompt("Enter your name");
-var roll_no = prompt("Enter your Rolll no.");
-var mobile_no = prompt("Enter your mobile no.");
-alert(name + roll_no + mobile_no);
+// var name = prompt("Enter your name");
+// var roll_no = prompt("Enter your Roll no.");
+// var mobile_no = prompt("Enter your mobile no.");
+// alert("NAME: "+ name + "\nROLL NO.:" + roll_no + "\nMOBILE NO.:" + mobile_no);
+
 
 // Homework 1: Take radius from the user.
 // Calculate circumference: 2 * PIE * radius
 // Calculate surface area: PIE(3.14) * radius * radius
 // Display Circumference and surface area.
+// var radius = Number(prompt("Enter radius:"));
+// var circumference = 2 * 3.14 * radius;
+// var surfaceArea = 3.14 * radius * radius;
+// alert("Circumference: " + circumference + "\nSurface Area: " + surfaceArea);
 
 
 
@@ -36,27 +41,54 @@ alert(name + roll_no + mobile_no);
 // var A = 50;
 // var B = 100;
 
-if(A > B){
-	alert("A is largest, B is smallest.")
-}x
+// if(A > B){
+// 	alert("A is largest, B is smallest.");
+// }
 
-if(A < B){
-	alert("B is largest, A is smallest.")
-}
+// if(A < B){
+// 	alert("B is largest, A is smallest.");
+// }
 
 
 // Exercise 5
 // Ask user to input A and B. Then display which number is smallest and largest.
-var A = prompt("Enter number A:");
-var B = prompt("Enter number B:");
+// var A = Number(prompt("Enter number A:"));
+// var B = Number(prompt("Enter number B:"));
 
-if(A > B){
-	alert("A is largest, B is smallest.");
-}
+// if(A > B){
+// 	alert("A is largest, B is smallest.");
+// }
 
-if(A < B){
-	alert("B is largest, A is smallest.");
-}
+// if(A < B){
+// 	alert("B is largest, A is smallest.");
+// }
 
 // Homework 2: 
-// Ask user to input A,B & C. Then display which number is smallest and largest.
+// Ask user to input A, B & C. Then display which number is smallest and largest.
+// A -> 11
+// B -> 10
+// C -> 5
+// Check if A is greater than (B & C), then display A is greater.
+// Check if B is greater than (A & C), then display B is greater.
+// Check if C is greater than (A & B), then display C is greater.
+
+// Solution to Homework 2:
+// STEP 1: Take A,B & C from the user.
+var A = Number(prompt("Enter A:"));
+var B = Number(prompt("Enter B:"));
+var C = Number(prompt("Enter C:"));
+
+// STEP 2: Check if A is greater than (B & C), then display A is greater.
+if(A > B && A > C){
+	alert("A is greater!")
+}
+
+// STEP 3: Check if B is greater than (A & C), then display B is greater.
+if(B > A && B > C){
+	alert("B is greater!")
+}
+
+// STEP 4: Check if C is greater than (A & B), then display C is greater.
+if(C > A && C > B){
+	alert("C is greater!")
+}
---
+++ class_3/script.js
@@ -0,0 +1,54 @@
+// HOMEWORK 4
+// Create a program showing a specific message:
+// -----------------------------------------------------
+// | Age range | Comment example                      |
+// | 1 to 17   | You are not a major yet.             |
+// | 18 to 49  | You are major, but not senior yet.   |
+// | 50 to 64  | You are senior, but not yet retired. |
+// ___________________________________________________ --
+// Algorithm:
+// STEP 1: Take age from user.
+// STEP 2: if age is between 1 to 17, display: You are not a major yet.
+// STEP 3: if age is between 18 to 49 , display: You are major, but not senior yet.
+// STEP 4: if age is between 50 to 64, display: You are senior, but not yet retired.
+// END
+
+// SOLUTION for HOMEWORK 4 STARTS HERE
+
+// SOLUTION for HOMEWORK 4 ENDS HERE
+
+
+// COURSE 4:
+// WHAT ARE LOOPS?
+// ITS TYPES?: while, do-while, for
+
+// Start from 1
+// alert("Hello");
+// End from 5001
+
+
+// Exercise 6:
+// Display "Hello" 4 times to the user.
+// alert("Hello");
+// alert("Hello");
+// alert("Hello");
+// alert("Hello");
+
+
+// Exercise 7:
+// Display "Hello" 10 times to the user by using while loop.
+// start -> 1 and end -> 10
+var i = 1;
+while(i <= 10){   // start -> 1 and i is smaller than or equal to 10
+    alert("Hello"); // Actual code. i = 1
+    i = i + 1; // i = 1 + 1 => i = 2
+} // i = 2 stop & go to start at line 42 
+
+
+// Homework 5:
+// Display "Bonjour" 25 times to the user by using while loop.
+// start -> 1 and end -> 25
+
+// SOLUTION for HOMEWORK 5 STARTS HERE
+
+// SOLUTION for HOMEWORK 5 ENDS HERE
