3988a3c71014aaedb2ec75866d092f04dbe7feb1
flockbithamp <flockbithamp@gmail.com>
2020-08-30 16:18:16 -0400

    Added noted for class 2

---
+++ Assignment_1.md
@@ -0,0 +1,5 @@
+# Assignment no 1
+
+Create similar functionality which is covered in class 1.
+In this, you have to hide/show h1 tag.
+
---
+++ class_2/script.js
@@ -0,0 +1,11 @@
+function hideParagraph(){
+    $("p").hide();
+    $("#hideParagraph").hide();
+    $("#showParagraph").show();
+}
+
+function showParagraph(){
+    $("p").show();
+    $("#hideParagraph").show();
+    $("#showParagraph").hide();
+}
