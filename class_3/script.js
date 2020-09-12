621cd1d86bc57ced39748689f9a5de8565fe8082
Jaspreet Singh Kalsi <bugsbunny@MacBook-Pro.local>
2020-09-12 16:27:59 -0400

    Uploaded assignment no 2

---
+++ class_3/script.js
@@ -0,0 +1,13 @@
+function hideParagraph(){
+    $('#hideShow').hide();
+}
+
+function showParagraph(){
+    $('#hideShow').show();
+}
+
+function showDiv(){
+    $("#div1").fadeIn();
+    $("#div2").fadeIn("slow");
+    $("#div3").fadeIn(3000);
+}
