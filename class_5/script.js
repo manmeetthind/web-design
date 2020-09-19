86531e76763171c35f3ef415c0880dc05031722e
flockbithamp <flockbithamp@gmail.com>
2020-09-19 16:01:58 -0400

    Uploading class_5 and assignment_3

---
+++ class_5/script.js
@@ -0,0 +1,11 @@
+function submitInfo() {
+    var userName = $("#userName").val();
+    var userEmail = $("#userEmail").val();
+    $("#informationBoard").empty();
+    displayInformation(userName, userEmail);
+}
+
+function displayInformation(userName, userEmail) {
+    $("#informationBoard").append('<li>Name: ' + userName + '</li>');
+    $("#informationBoard").append('<li>Email id: ' + userEmail + '</li>')
+}
